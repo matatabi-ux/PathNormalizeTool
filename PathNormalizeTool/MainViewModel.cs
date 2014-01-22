37d9d1c178b649d119362a1757371cefc51e315e
Tatsuji Kuroyanagi <tatsuji_kuroyanagi@2fc-group.jp>
2014-01-23 05:50:26 +0900

    first commit

---
+++ PathNormalizeTool/MainViewModel.cs
@@ -0,0 +1,246 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace PathNormalizeTool
+{
+    /// <summary>
+    /// メイン ViewModel
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// 入力済みフラグ
+        /// </summary>
+        public bool IsInputted { get { return !string.IsNullOrEmpty(this.pathData); } }
+
+        #region PathData プロパティ
+        /// <summary>
+        /// Path Data
+        /// </summary>
+        private string pathData;
+
+        /// <summary>
+        /// Path Data
+        /// </summary>
+        public string PathData
+        {
+            get { return this.pathData; }
+            set
+            {
+                this.Set<string>("PathData", ref this.pathData, value);
+                this.Geometry = Geometry.Parse(this.pathData);
+                this.Width = (this.Geometry.Bounds.Right - this.Geometry.Bounds.Left);
+                this.Height = (this.Geometry.Bounds.Bottom - this.Geometry.Bounds.Top);
+                this.UpdatePathData();
+                this.RaisePropertyChanged("PathData");
+                this.RaisePropertyChanged("IsInputted");
+            }
+        }
+        #endregion //PathData プロパティ
+
+        #region Width プロパティ
+        /// <summary>
+        /// Width
+        /// </summary>
+        private double width;
+
+        /// <summary>
+        /// Width
+        /// </summary>
+        public double Width
+        {
+            get { return this.width; }
+            set 
+            { 
+                this.Set<double>("Width", ref this.width, value);
+                this.UpdatePathData();
+            }
+        }
+        #endregion //Width プロパティ
+
+        #region Height プロパティ
+        /// <summary>
+        /// Height
+        /// </summary>
+        private double height;
+
+        /// <summary>
+        /// Height
+        /// </summary>
+        public double Height
+        {
+            get { return this.height; }
+            set 
+            { 
+                this.Set<double>("Height", ref this.height, value);
+                this.UpdatePathData();
+            }
+        }
+        #endregion //Height プロパティ
+
+        #region Left プロパティ
+        /// <summary>
+        /// Left
+        /// </summary>
+        private double left;
+
+        /// <summary>
+        /// Left
+        /// </summary>
+        public double Left
+        {
+            get { return this.left; }
+            set
+            {
+                this.Set<double>("Left", ref this.left, value);
+                this.UpdatePathData();
+                this.RaisePropertyChanged("Margin");
+            }
+        }
+        #endregion //Left プロパティ
+
+        #region Top プロパティ
+        /// <summary>
+        /// Top
+        /// </summary>
+        private double top;
+
+        /// <summary>
+        /// Top
+        /// </summary>
+        public double Top
+        {
+            get { return this.top; }
+            set
+            {
+                this.Set<double>("Top", ref this.top, value);
+                this.UpdatePathData();
+                this.RaisePropertyChanged("Margin");
+            }
+        }
+        #endregion //Top プロパティ
+
+        #region Right プロパティ
+        /// <summary>
+        /// Right
+        /// </summary>
+        private double right;
+
+        /// <summary>
+        /// Right
+        /// </summary>
+        public double Right
+        {
+            get { return this.right; }
+            set
+            {
+                this.Set<double>("Right", ref this.right, value);
+                this.RaisePropertyChanged("Margin");
+            }
+        }
+        #endregion //Right プロパティ
+
+        #region Bottom プロパティ
+        /// <summary>
+        /// Bottom
+        /// </summary>
+        private double bottom;
+
+        /// <summary>
+        /// Bottom
+        /// </summary>
+        public double Bottom
+        {
+            get { return this.bottom; }
+
+            set
+            {
+                this.Set<double>("Bottom", ref this.bottom, value);
+                this.RaisePropertyChanged("Margin");
+            }
+        }
+        #endregion //Bottom プロパティ
+
+        /// <summary>
+        /// Margin
+        /// </summary>
+        public Thickness Margin
+        {
+            get { return new Thickness(0, 0, this.left, this.top); }
+        }
+
+        /// <summary>
+        /// Geometry
+        /// </summary>
+        public Geometry Geometry { get; set; }
+
+        /// <summary>
+        /// Path の整形
+        /// </summary>
+        public void UpdatePathData()
+        {
+            try
+            {
+                if (this.Geometry == null)
+                {
+                    return;
+                }
+
+                var pathGeometory = (this.Geometry.Clone() as StreamGeometry).GetFlattenedPathGeometry();
+                var scaled = new PathGeometry();
+                double scaleX = (this.Geometry.Bounds.Right - this.Geometry.Bounds.Left) / this.Width;
+                double scaleY = (this.Geometry.Bounds.Bottom - this.Geometry.Bounds.Top) / this.Height;
+
+                foreach (var figure in pathGeometory.Figures)
+                {
+                    var newFigure = new PathFigure();
+
+                    newFigure.StartPoint = new Point(
+                        ((figure.StartPoint.X - this.Geometry.Bounds.Left) / scaleX) + this.Left,
+                        ((figure.StartPoint.Y - this.Geometry.Bounds.Top) / scaleY) + this.Top
+                        );
+                    foreach (PolyLineSegment segment in figure.Segments)
+                    {
+                        var newSegment = new PolyLineSegment();
+                        foreach (var point in segment.Points)
+                        {
+                            newSegment.Points.Add(new Point(
+                                ((point.X - this.Geometry.Bounds.Left) / scaleX) + this.Left,
+                                ((point.Y - this.Geometry.Bounds.Top) / scaleY) + this.Top
+                                ));
+                        }
+                        newFigure.Segments.Add(newSegment);
+                    }
+                    scaled.Figures.Add(newFigure);
+                }
+                scaled.Freeze();
+                this.pathData = scaled.ToString();
+                this.RaisePropertyChanged("PathData");
+                this.PathGeometry = scaled;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        /// <summary>
+        /// 整形後 Path
+        /// </summary>
+        private Geometry pathGeometry;
+
+        /// <summary>
+        /// 整形後 Path
+        /// </summary>
+        public Geometry PathGeometry
+        {
+            get { return this.pathGeometry; }
+            set { this.Set<Geometry>("PathGeometry", ref this.pathGeometry, value); }
+        }
+    }
+}
---
+++ PathNormalizeTool/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PathNormalizeTool
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// ViewModel
+        /// </summary>
+        public MainViewModel ViewModel
+        {
+            get { return this.DataContext as MainViewModel; }
+        }
+
+        /// <summary>
+        /// テキストフォーカスイベントハンドラ
+        /// </summary>
+        /// <param name="sender">イベント発行者</param>
+        /// <param name="e">イベント引数</param>
+        private void OnTextBoxGotFocus(object sender, RoutedEventArgs e)
+        {
+            var textbox = sender as TextBox;
+            if (textbox != null)
+            {
+                Action act = textbox.SelectAll;
+                Dispatcher.BeginInvoke(act);
+            }
+        }
+
+        /// <summary>
+        /// キー押下イベントハンドラ
+        /// </summary>
+        /// <param name="sender">イベント発行者</param>
+        /// <param name="e">イベント引数</param>
+        private void OnTextBoxKeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = false;
+            
+            var textbox = sender as TextBox;
+            double value = 0;
+            if (textbox != null && double.TryParse(textbox.Text, out value))
+            {
+                var expression = BindingOperations.GetBindingExpression(textbox, TextBox.TextProperty);
+
+                if ((Keyboard.Modifiers == ModifierKeys.None) && (e.Key == Key.Up))
+                {
+                    textbox.Text = (value + 1).ToString();
+                    expression.UpdateSource();
+                    e.Handled = true;
+                }
+                if ((Keyboard.Modifiers == ModifierKeys.None) && (e.Key == Key.Down))
+                {
+                    textbox.Text = (value - 1).ToString();
+                    expression.UpdateSource();
+                    e.Handled = true;
+                }
+                if ((Keyboard.Modifiers == ModifierKeys.None) && (e.Key == Key.Enter))
+                {
+                    this.ViewModel.UpdatePathData();
+                    e.Handled = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// コピーボタン押下イベントハンドラ
+        /// </summary>
+        /// <param name="sender">イベント発行者</param>
+        /// <param name="e">イベント引数</param>
+        private void OnCopyButtonClick(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(this.ViewModel.PathData);
+        }
+    }
+}
